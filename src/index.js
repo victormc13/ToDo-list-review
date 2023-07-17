793fe84f0a6670bbacec0ac0a0ac82b7b6fef8d6
victormc13 <viktorsb14@gmail.com>
2023-07-17 14:03:39 -0500

    Code ready for review

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import './style.css';
+import { renderTasks, addTask, taskMenu } from './modules/tasks.js';
+
+window.addEventListener('DOMContentLoaded', () => {
+  renderTasks();
+  taskMenu();
+});
+
+const form = document.querySelector('.task-form');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const input = form.querySelector('input[type=text]');
+  const description = input.value.trim();
+  if (description) {
+    addTask(description);
+    input.value = '';
+  }
+  // taskMenu();
+});
+
+const addTaskBtn = document.querySelector('.la-level-down-alt');
+addTaskBtn.addEventListener('click', () => {
+  const input = form.querySelector('input[type=text]');
+  const description = input.value.trim();
+  if (description) {
+    addTask(description);
+    input.value = '';
+  }
+  taskMenu();
+});
+
+const refreshTasks = document.querySelector('.la-sync');
+refreshTasks.addEventListener('click', () => {
+  window.location.reload();
+});
